2a0b23138d7efec08455e73ac35acff3512224c4
philippetsongo <philippetsongo90@gmail.com>
2025-03-20 23:28:25 +0200

    use the production base url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Set up Axios instance with the base URL
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000", // Base URL
+    // baseURL: "http://127.0.0.1:8000", // loacal base URL
+    baseURL: "http://trip-logger-backend-production.up.railway.app" //production base URL
 });
 
 // Function to fetch trips

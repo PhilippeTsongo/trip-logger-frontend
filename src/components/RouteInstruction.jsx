e209754e67a9aeaf9f5c94641debde6c9dc1cb4b
philippetsongo <philippetsongo90@gmail.com>
2025-04-15 22:21:23 +0200

    review of the ui of the route instruction component

--- src/components/RouteInstruction.jsx
+++ src/components/RouteInstruction.jsx
@@ -1,7 +1,7 @@
 const RouteInstruction = ({ instruction }) => {
     return (
-        <div className="bg-gray-100 rounded-md px-2 py-2 text-sm flex flex-col hover:bg-[#5ead8a] hover:text-white transition">
-            <div className="flex justify-between text-sm mb-3 text-[#5ead8a] hover:text-white">
+        <div className="bg-gray-100 rounded-md px-2 py-2 text-sm flex flex-col border border-gray-300 hover:border hover:border-gray-400 transition">
+            <div className="flex justify-between text-sm mb-3 text-[#5ead8a]">
                 <strong>{instruction.instruction}</strong>
                 <div className="text-xs">{instruction.duration}'</div>
             </div>
